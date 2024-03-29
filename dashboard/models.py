7309fe9f903a3b941b74cd4d180d218d42fe7c17
sinasezza <s.e.sezza121@gmail.com>
2024-03-29 11:37:26 +0330

    feat(dashboard): country models and form created and charts added to index page.

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+
+class CountryData(models.Model):
+    country = models.CharField(max_length=100)
+    population = models.PositiveSmallIntegerField()
+    
+    class Meta:
+        verbose_name_plural = 'Countries Population Data'
+        
+    def __str__(self) -> str:
+        return f"{self.country}: {self.population}"
+    
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CountryData
+from .forms import CountryDataForm
 
 
 
 def index(request):
-    context = {}
+    form = CountryDataForm(request.POST or None)
+    
+    if form.is_valid():
+        data = form.save()
+        return redirect('dashboard:index')
+    
+    countries = CountryData.objects.all()
+    context = {
+        'form': form,
+        'countries': countries,
+    }
     return render(request, 'index.html', context)
